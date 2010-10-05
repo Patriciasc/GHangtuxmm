fa41393c047ed0e023e6c007a8de4eb5e440d072
Patricia Santana Cruz <patriciasc@openismus.com>
2010-10-05 12:02:29 +0200

    Changed foreach Slot's name function for a more sensible one.

--- src/ghangtuxmm_keyboard.h
+++ src/ghangtuxmm_keyboard.h
@@ -43,7 +43,7 @@ public:
 
 private:
     void on_button_clicked(Gtk::Button* button);
-    void on_set_sensitive(Gtk::Widget& button, const bool& sensitive);
+    void on_foreach_set_sensitive(Gtk::Widget& button, const bool& sensitive);
 
     T_signal_clicked m_on_button_clicked;
 };
